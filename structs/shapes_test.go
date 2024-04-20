5b1d9b2fc58354d42da06098bc2deec0773bff4e
kiLeo13 <leonardo.silvavieira@hotmail.com>
2024-04-20 01:59:32 -0300

    Introducing Structs, Methods and Interfaces.
    * Created interface "Shape" for better abstraction and parameter usage.
    * Created structs "Rectangle" and "Circle"

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,36 +1,49 @@
 package main
 
-import "testing"
-
-func TestPerimeter(t *testing.T) {
-    rectangle := Rectangle{10.0, 10.0}
-    got := Perimeter(rectangle)
-    want := 40.0
-
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
-    }
-}
+import (
+	"math"
+	"testing"
+)
 
 func TestArea(t *testing.T) {
-    rectangle := Rectangle{12.0, 6.0}
-    got := Area(rectangle)
-    want := 72.0
 
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
+    checkArea := func(t testing.TB, shape Shape, want float64)  {
+        t.Helper()
+        got := shape.Area()
+
+        if got != want {
+            t.Errorf("got %g want %g", got, want)
+        }
     }
+
+    t.Run("rectangles", func(t *testing.T) {
+        rectangle := Rectangle{12.0, 6.0}
+        checkArea(t, rectangle, 72.0)
+    })
+
+    t.Run("circles", func(t *testing.T) {
+        circle := Circle{10}
+        checkArea(t, circle, 314.1592653589793)
+    })
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-    return 2 * (rectangle.Width + rectangle.Height)
-}
-
-func Area(rectangle Rectangle) float64 {
-    return rectangle.Width * rectangle.Height
+type Shape interface {
+    Area() float64
 }
 
 type Rectangle struct {
     Width   float64
     Height  float64
+}
+
+func (r Rectangle) Area() float64 {
+    return r.Width * r.Height
+}
+
+type Circle struct {
+    Radius float64
+}
+
+func (c Circle) Area() float64 {
+    return math.Pi * c.Radius * c.Radius
 }
