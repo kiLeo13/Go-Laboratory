89cdb0c28314c57046ddef6bbc0485e793a83d9d
kiLeo13 <leonardo.silvavieira@hotmail.com>
2024-04-20 02:32:47 -0300

    Update shapes_test.go
    * Implement usage of Table Driven Tests.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -7,24 +7,26 @@ import (
 
 func TestArea(t *testing.T) {
 
-    checkArea := func(t testing.TB, shape Shape, want float64)  {
-        t.Helper()
-        got := shape.Area()
-
-        if got != want {
-            t.Errorf("got %g want %g", got, want)
-        }
+    areaTests := []struct {
+        name    string
+        shape   Shape
+        hasArea float64
+    }{
+        {name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+        {name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+        {name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
     }
 
-    t.Run("rectangles", func(t *testing.T) {
-        rectangle := Rectangle{12.0, 6.0}
-        checkArea(t, rectangle, 72.0)
-    })
+    for _, tt := range areaTests {
 
-    t.Run("circles", func(t *testing.T) {
-        circle := Circle{10}
-        checkArea(t, circle, 314.1592653589793)
-    })
+        t.Run(tt.name, func(t *testing.T) {  
+            got := tt.shape.Area()
+    
+            if got != tt.hasArea {
+                t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+            }
+        })
+    }
 }
 
 type Shape interface {
@@ -46,4 +48,13 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
     return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+    Base   float64
+    Height float64
+}
+
+func (t Triangle) Area() float64 {
+    return (t.Base * t.Height) / 2.0
 }
