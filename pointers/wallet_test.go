9da41979b4204251adbbf1d37ff20ad0759d8906
kiLeo13 <leonardo.silvavieira@hotmail.com>
2024-04-20 22:00:17 -0300

    Introducing pointers and error handling.
    * Created a Wallet struct and methods to deposit/withdraw given amounts. These methods required the usage of pointers.
    * Withdraw method may return an error if the amount provided is greater than the current balance. Which is properly handled by the caller.

---
+++ pointers/wallet_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func TestWallet(t *testing.T) {
+
+    t.Run("deposit", func(t *testing.T) {
+        wallet := Wallet{}
+        wallet.Deposit(Bitcoin(10))
+        
+        assertBalance(t, wallet, Bitcoin(10))
+    })
+    
+    t.Run("withdraw with funds", func(t *testing.T) {
+        wallet := Wallet{Bitcoin(20)}
+        err := wallet.Withdraw(Bitcoin(10))
+
+        assertNoError(t, err)
+        assertBalance(t, wallet, Bitcoin(10))
+    })
+
+    t.Run("withdraw insufficient funds", func(t *testing.T) {
+        startingBalance := Bitcoin(20)
+        wallet := Wallet{startingBalance}
+        err := wallet.Withdraw(Bitcoin(100))
+
+        assertError(t, err, ErrInsufficientFunds)
+        assertBalance(t, wallet, startingBalance)
+    })
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+    t.Helper()
+    got := wallet.Balance()
+
+    if got != want {
+        t.Errorf("got %s want %s", got, want)
+    }
+}
+
+func assertNoError(t testing.TB, got error) {
+    t.Helper()
+
+    if got != nil {
+        t.Fatal("got an error but didn't want one")
+    }
+}
+
+func assertError(t testing.TB, got, want error) {
+    t.Helper()
+
+    if got == nil {
+        t.Fatal("wanted get error but wanted one")
+    }
+
+    if got != want {
+        t.Errorf("got %q, want %q", got, want)
+    }
+}
+
+type Bitcoin int
+
+type Stringer interface {
+    String() string
+}
+
+func (b Bitcoin) String() string {
+    return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+    balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+    w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+    if amount > w.balance {
+        return ErrInsufficientFunds
+    }
+
+    w.balance -= amount
+    return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+    return w.balance
+}
